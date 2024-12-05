7e0220a398c0bd1fc60ffaf53c5f6041ed007e0a
Lunitur <karlo.puselj@gmail.com>
2024-12-05 13:16:09 +0100

    day 5

---
+++ haskell/day5.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+import Control.Monad (void)
+import Data.Either (fromRight)
+import Data.Function (on, (&))
+import Data.Functor ((<&>))
+import Data.List qualified as List
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Set (Set)
+import Data.Set qualified as Set
+import Text.Parsec
+import Text.RawString.QQ
+
+example =
+  [r|47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47|]
+
+inputParser :: Parsec String () (Set (Int, Int), [[Int]])
+inputParser = do
+  orders <- many1 order
+  newline
+  updates <- many1 update
+  return (Set.fromList orders, updates)
+
+number = do
+  i <- many1 digit
+  return (read i :: Int)
+
+update = do
+  pages <- sepBy1 number (char ',')
+  void endOfLine <|> eof
+  return pages
+
+order = do
+  i1 <- number
+  char '|'
+  i2 <- number
+  endOfLine
+  return (i1, i2)
+
+part1 (os, us) = correctUpdates <&> (\xs -> xs !! (length xs `div` 2)) & sum
+  where
+    correctUpdates = filter (isCorrect os) us
+
+isCorrect :: Set (Int, Int) -> [Int] -> Bool
+isCorrect os u = and [(h, j) `Set.member` os || (j, h) `Set.notMember` os | i@(h : _) <- List.tails u, j <- tail i]
+
+part2 (os, us) = (incorrectUpdates <&> (\xs -> xs !! (length xs `div` 2)) . order) & sum
+  where
+    order = List.sortBy cmp
+    incorrectUpdates = filter (not . isCorrect os) us
+    cmp x y
+      | (x, y) `Set.member` os = LT
+      | (y, x) `Set.member` os = GT
+      | otherwise = EQ
